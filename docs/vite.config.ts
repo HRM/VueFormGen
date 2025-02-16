cdd7ce50063f5591e9b68923f00f8010d140b3f0
Tamás Török <tamas.torok.w@outlook.com>
2025-02-16 22:13:10 +0200

    feat(docs): change base path

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const PUBLIC_PATH = process.env.NODE_ENV === 'production' ? '/VueFormGen/' : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: PUBLIC_PATH,
   plugins: [
     vue(),
     vueDevTools(),
